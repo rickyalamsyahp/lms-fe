e2af167d00cbc6f5993af7a5caafb0f30b8787c5
dwiargodirgantoro <dwiargodirgantoro@gmail.com>
2024-08-23 10:49:37 +0700

    renamve course exan form

--- src/modules/dashboard/courseExam/CourseExamDetailOverview.tsx
+++ src/modules/dashboard/courseExam/CourseExamDetailOverview.tsx
@@ -2,7 +2,7 @@ import { Box, Paper } from '@mui/material'
 import { useParams } from 'react-router-dom'
 import JumpingLoader from '../../../components/shared/JumpingLoader'
 import { useSession } from '../../../context/session'
-import UserForm from './__components/CourseExamForm'
+import CourseExamForm from './__components/CourseExamForm'
 import { useCourseExam } from './__shared/api'
 
 export default function UserDetailOverview() {
@@ -13,7 +13,7 @@ export default function UserDetailOverview() {
   return user ? (
     <Box maxWidth={'sm'}>
       <Paper>
-        <UserForm initialData={user} asDialog={false} />
+        <CourseExamForm initialData={user} asDialog={false} />
       </Paper>
     </Box>
   ) : (
